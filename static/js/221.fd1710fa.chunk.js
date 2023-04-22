0e26bda37b20a8ab786086ef91fca900cfd47d40
Hatrison <90156655+Hatrison@users.noreply.github.com>
2023-04-22 00:05:29 +0000

    Deploying to gh-pages from @ Hatrison/goit-react-hw-08-phonebook@0801335a16ccb1026dafe232fc1ddf55f5d2b35c 🚀

---
+++ static/js/221.fd1710fa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[221],{9221:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var c,r=n(168),a=n(6444).ZP.div(c||(c=(0,r.Z)(["\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  height: calc(100vh - 100px);\n"]))),o=n(184),i=function(){return(0,o.jsx)(a,{children:(0,o.jsx)("b",{children:"Welcome to my contact book app!\ud83d\ude4b\u200d\u2642\ufe0f"})})}}}]);
+//# sourceMappingURL=221.fd1710fa.chunk.js.map
